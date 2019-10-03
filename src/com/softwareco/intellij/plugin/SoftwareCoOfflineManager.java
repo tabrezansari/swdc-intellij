a7d7e87e0d7110224d2b95e6d056b4e224cec0cd
kkadu <kkadu@microproindia.com>
2019-10-03 10:48:33 +0530

    SFTW-1981 Dashboard displayed properly

--- src/com/softwareco/intellij/plugin/SoftwareCoOfflineManager.java
+++ src/com/softwareco/intellij/plugin/SoftwareCoOfflineManager.java
@@ -117,7 +117,7 @@ public class SoftwareCoOfflineManager {
         if (f.exists()) {
             try {
                 byte[] encoded = Files.readAllBytes(Paths.get(sessionSummaryFile));
-                String content = new String(encoded, Charset.defaultCharset());
+                String content = new String(encoded, Charset.forName("UTF-8"));
                 if (content != null) {
                     // json parse it
                     data = SoftwareCo.jsonParser.parse(content).getAsJsonObject();
@@ -137,7 +137,7 @@ public class SoftwareCoOfflineManager {
         if (f.exists()) {
             try {
                 byte[] encoded = Files.readAllBytes(Paths.get(sessionSummaryFile));
-                content = new String(encoded, Charset.defaultCharset());
+                content = new String(encoded, Charset.forName("UTF-8"));
             } catch (Exception e) {
                 log.warning("Code Time: Error trying to read and json parse the session file, error: " + e.getMessage());
             }
